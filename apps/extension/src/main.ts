b7b292044d452456fa722705df5a81b2273d2fe7
Thomas Breindl <44656811+Aquitano@users.noreply.github.com>
2022-10-02 16:52:01 +0200

    feat(extension): Save articles with the browser extension (#36)
    * feat(dependabot): Change settings for Gitflow
    
    * chore(deps-dev): bump tsconfig-paths from 3.14.1 to 4.1.0 (#4)
    
    * fix(deps): bump eslint-config-airbnb-typescript from 16.2.0 to 17.0.0 (#14)
    
    * fix(deps): bump @nestjs/core from 8.4.7 to 9.0.11 (#2)
    
    * fix(deps): bump @nestjs/platform-express from 8.4.7 to 9.0.11 (#1)
    
    * chore(deps-dev): bump @nestjs/cli from 8.2.8 to 9.1.1 (#5)
    
    * feat(extension): Add save ability
    
    * fix(extension): Run listener only once
    
    * chore(Restyle) - feat(extension): Save articles with the browser extension (#37)
    
    * Restyled by jq
    
    * Restyled by prettier-json
    
    * Restyled by prettier-yaml
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- apps/extension/src/main.ts
+++ apps/extension/src/main.ts
@@ -1,5 +1,48 @@
-import { setupButton } from './button';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { setupLoginButton, setupSaveButton } from './button';
 import './style.css';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-setupButton(document.querySelector<HTMLButtonElement>('#article')!);
+// Get encoded data from JWT token
+function parseJwt(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map((c) => {
+        return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+      })
+      .join(''),
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+// Check if user is logged in
+chrome.storage.local.get(['jwt'], (result) => {
+  const button = document.querySelector<HTMLButtonElement>('#main-button')!;
+
+  if (result.jwt && Date.now() <= parseJwt(result.jwt).exp * 1000) {
+    button.innerText = 'Save Article';
+    button.id = 'save-button';
+
+    const inputs = document.querySelectorAll('.wrap-input');
+    inputs.forEach((input) => {
+      input.remove();
+    });
+
+    setupSaveButton(button, result.jwt);
+  } else {
+    button.innerText = 'Login';
+    button.id = 'login-button';
+    button.addEventListener('click', () => {
+      chrome.runtime.sendMessage({ type: 'login' });
+      const credentials = {
+        email: document.querySelector<HTMLInputElement>('#email')!.value,
+        password: document.querySelector<HTMLInputElement>('#password')!.value,
+      };
+      setupLoginButton(button, credentials);
+    });
+  }
+});
